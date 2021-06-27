32328c3ffbc2fe2c17f192123e2c82f2f8e23dc0
Pooja B <53658738+poojabasker@users.noreply.github.com>
2021-06-27 15:27:51 +0530

    Update server.js

--- server.js
+++ server.js
@@ -1,21 +1,12 @@
 var express = require('express')
-//var bodyParser = require('body-parser')
 var app = express()
-//http module from node is used
-//app is passed to the server
 var http = require('http').Server(app)
-//passing http to socket.io
 var io = require('socket.io')(http)
 var mongoose = require('mongoose')
-//if no port is fixed then 5000
-//process.env.PORT - any port heroku works
 var port = process.env.PORT || 3000
 
-// For parsing application/x-www-form-urlencoded
 app.use(express.urlencoded({ extended : false }))
-// For parsing application/json
 app.use(express.json())
-//using a static file, entire directory instead of separate file
 app.use(express.static(__dirname))
 
 var dburl = 'mongodb+srv://<username>:<password>@cluster1.i5ksw.mongodb.net/<dbname>?retryWrites=true&w=majority'
@@ -26,9 +17,8 @@ var Message = mongoose.model('Message', {
 })
 
 app.get('/messages', (req,res)=>{
-    //empty requirements so all the messages in the database is selected
     Message.find({}, (err,messages)=>{
-        res.send(messages) //dynamic messages
+        res.send(messages)
     })
 })
 
@@ -38,11 +28,8 @@ app.post('/messages', (req,res)=>{
         if(err)
             sendStatus(500)
         io.emit('message',req.body)
-        res.sendStatus(200) //200 - everything went OK
+        res.sendStatus(200) 
     })
-    //console.log(req.body) //the request sent
-    //this will display undefined in the console as express doesn't have built in support
-    //to make it happen, you need to install body parser
 })
 
 io.on('connection', (socket)=>{
@@ -53,7 +40,6 @@ mongoose.connect(dburl, (err)=>{
     console.log('MongoDB connected!')
 })
 
-//node http server is used so that both express and socket.io is running
 var server = http.listen(port, () =>{
     console.log("Application is listening on PORT", port)
 })
